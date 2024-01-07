93ec4bf10bbbe165f80a0f9fccc0bafe3fe7629c
dev-cassini <ryan.dremza@gmail.com>
2024-01-07 00:40:44 +0000

    examples - infrastructure - ef - domain model

---
+++ examples/infrastructure/ef/src/Audit.Domain/Model/Forecourt.cs
@@ -0,0 +1,22 @@
+namespace Audit.Domain.Model;
+
+public class Forecourt
+{
+    public Guid Id { get; }
+
+    private readonly List<Lane> _lanes = new();
+    public IEnumerable<Lane> Lanes => _lanes.AsReadOnly();
+
+    private readonly List<Transaction> _transactions = new();
+    public IEnumerable<Transaction> Transactions => _transactions.AsReadOnly();
+
+    protected Forecourt(Guid id)
+    {
+        Id = id;
+    }
+    
+    #region EF Constructor
+    // ReSharper disable once UnusedMember.Local
+    private Forecourt() { }
+    #endregion
+}
---
+++ examples/infrastructure/ef/src/Audit.Domain/Model/Pump.cs
@@ -0,0 +1,27 @@
+namespace Audit.Domain.Model;
+
+public class Pump
+{
+    /// <summary>
+    /// Fuel dispense rate in litres per second.
+    /// </summary>
+    public const decimal FuelDispenseRate = 1.5m;
+    
+    public virtual Guid Id { get; }
+    public Guid? VehicleId { get; protected set; }
+    
+    public Guid LaneId { get; }
+    public Lane Lane { get; } = null!;
+
+    protected Pump(Guid id, Lane lane)
+    {
+        Id = id;
+        LaneId = lane.Id;
+        Lane = lane;
+    }
+    
+    #region EF Constructor
+    // ReSharper disable once UnusedMember.Local
+    private Pump() { }
+    #endregion
+}
