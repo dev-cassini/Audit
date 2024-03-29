7049b6ecb4fc1cd2914a21416f88acf5496fc7be
dev-cassini <ryan.dremza@gmail.com>
2024-03-29 22:41:16 +0000

    examples - infrastructure - ef - infrastructure - read db context

---
+++ examples/infrastructure/ef/src/Audit.Infrastructure/Persistence/EntityFramework/Read/DbContext.cs
@@ -0,0 +1,34 @@
+using System.Reflection;
+using Audit.Domain.Abstraction.Model.Audit;
+using Audit.Domain.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Audit.Infrastructure.Persistence.EntityFramework.Read;
+
+public class DbContext : Microsoft.EntityFrameworkCore.DbContext
+{
+    private DbSet<Forecourt> Forecourts { get; set; } = null!;
+    public IQueryable<Forecourt> ForecourtsView => Forecourts.AsNoTracking();
+    
+    private DbSet<Lane> Lanes { get; set; } = null!;
+    public IQueryable<Lane> LanesView => Lanes.AsNoTracking();
+    
+    private DbSet<Pump> Pumps { get; set; } = null!;
+    public IQueryable<Pump> PumpsView => Pumps.AsNoTracking();
+    
+    private DbSet<PumpAuditRecord> PumpAuditRecords { get; set; } = null!;
+    public IQueryable<PumpAuditRecord> PumpAuditRecordsView => PumpAuditRecords.AsNoTracking();
+    
+    private DbSet<AuditRecordMetadata> PumpAuditRecordMetadata { get; set; } = null!;
+    public IQueryable<AuditRecordMetadata> PumpAuditRecordMetadataView => PumpAuditRecordMetadata.AsNoTracking();
+    
+    private DbSet<Vehicle> Vehicles { get; set; } = null!;
+    public IQueryable<Vehicle> VehiclesView => Vehicles.AsNoTracking();
+    
+    public DbContext(DbContextOptions<DbContext> options) : base(options) { }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+}
