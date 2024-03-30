d9aba1d0f9d5a629f11bbd7fafe9d5afd565014b
dev-cassini <ryan.dremza@gmail.com>
2024-03-30 00:15:06 +0000

    examples - infrastructure - ef - register services at api startup

--- examples/infrastructure/ef/src/Audit.Api/Program.cs
+++ examples/infrastructure/ef/src/Audit.Api/Program.cs
@@ -1,17 +1,50 @@
+using Audit.Api.Endpoints;
+using Audit.Domain;
+using Audit.Infrastructure;
+using Audit.Infrastructure.Persistence.EntityFramework;
+using EntityFramework = Audit.Infrastructure.Persistence.EntityFramework;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services
     .AddEndpointsApiExplorer()
-    .AddSwaggerGen();
+    .AddSwaggerGen()
+    .AddDomainServices()
+    .AddInfrastructure(infrastructureConfigurator =>
+    {
+        infrastructureConfigurator
+            .AddPersistence(persistenceConfigurator =>
+            {
+                persistenceConfigurator
+                    .AddEntityFramework(efConfigurator =>
+                    {
+                        efConfigurator
+                            .AddDbContext<EntityFramework.Write.DbContext>(
+                                builder.Configuration,
+                                dbConfigurator => dbConfigurator.UsePostgres(),
+                                interceptorConfigurator => interceptorConfigurator.AddAuditInterceptors());
+                        
+                        efConfigurator
+                            .AddDbContext<EntityFramework.Read.DbContext>(
+                                builder.Configuration,
+                                dbConfigurator => dbConfigurator.UsePostgres());
+                    });
+            })
+            .AddMessaging(messagingConfigurator => messagingConfigurator.AddMediatR());
+    });
 
 var app = builder.Build();
-
-app.MapGet("/", () => "Hello World!");
 
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.Services.MigrateDatabase();
+
+app
+    .RegisterEndpoints()
+    .UseHttpsRedirection();
 
 app.Run();
